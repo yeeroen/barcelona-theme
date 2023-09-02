a099184ea376a0a3982b6216d637c797629a9abb
yeeroen <31047133+yeeroen@users.noreply.github.com>
2023-09-02 11:51:09 +0200

    Added aliases

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@vemedia/article-highlight': path.resolve(__dirname, './src/components/bcn-article-highlight'),
+      '@vemedia/article-link': path.resolve(__dirname, './src/components/bcn-article-link'),
+      '@vemedia/breadcrumbs': path.resolve(__dirname, './src/components/bcn-breadcrumbs'),
+      '@vemedia/button': path.resolve(__dirname, './src/components/bcn-button'),
+    },
+  },
 })
