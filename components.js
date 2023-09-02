fd639ad0084c49b470e1457bc8ba1f6d6b52add7
yeeroen <31047133+yeeroen@users.noreply.github.com>
2023-09-02 10:46:49 +0200

    Added export

--- components.js
+++ components.js
@@ -1,5 +1,5 @@
-import ArticleLink from './src/components/bcn-article-link/bcn-article-link.vue';
+import BcnArticleLink from './src/components/bcn-article-link/bcn-article-link.vue';
 
 export default {
-    ArticleLink,
+    BcnArticleLink,
 };
